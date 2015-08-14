fcd9ccac5f32de0f25d6953f92afd20525f3d254
Ani Kembhavi <anik@allenai.org>
2015-08-13 23:34:27 -0700

    5

--- src/data.c
+++ src/data.c
@@ -626,7 +626,7 @@ data load_data_tables(char **paths, int n, int m, int w, int h, char *dataFolder
     d.X = load_image_paths(paths, n, w, h);
     printf("4");
     fflush(stdout);
-    d.y = load_csv_paths(paths, n);
+    d.y = load_csv_paths(replace_paths, n);
     printf("5");
     fflush(stdout);
     if(m) free(paths);
